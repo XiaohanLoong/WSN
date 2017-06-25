b7b33a52a9fce4064bf29d78f61038d08ebd2ac8
loongxiaohan <loongxiaohan@sina.com>
2017-06-25 11:17:18 +0800

    测试用，暂时不准备提交代码

--- Test.py
+++ Test.py
@@ -1,9 +1,2 @@
 #encoding:utf8
 print 'HelloPython . Good bye MATLAB . '
-result = 0
-for i in range(1000):
-    result += i + 1
-print result
-print 'There is a bug in your code ! '
-print 'I have fixed the bug . '
-print '我在dev分支上进行一些工作，还没有完成就被叫过去修复master分支上的bug了.'
